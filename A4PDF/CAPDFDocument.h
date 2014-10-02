a8316b49dbbb0fac506139f09661b4ec29f3af48
Cameron Milsom <Cameron.Milsom@gmail.com>
2014-10-02 20:35:54 +1300

    Initial Commit

---
+++ A4PDF/CAPDFDocument.h
@@ -0,0 +1,13 @@
+//
+//  CAPDFDocument.h
+//  A4PDF
+//
+//  Created by Cameron Milsom on 10/2/14.
+//  Copyright (c) 2014 cmilsom. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CAPDFDocument : NSDocument
+
+@end
